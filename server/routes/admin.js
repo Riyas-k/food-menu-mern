d7dfd29e50604d463b0e5a5b70c879e6041176a9
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-07-19 11:53:46 +0530

    gitignore added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,8 +6,7 @@ const adminController = require("../controllers/admin-controller");
 
 router.get("/", adminController.getUsers);
 router.post("/login", adminController.postAdmin);
-// router.put("/delete-user/:id", adminController.deleteUser);
-// router.post('/add-user',adminController.addUser);
+
 
 //admin menu related routes
 
